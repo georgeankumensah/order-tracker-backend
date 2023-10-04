e8360600b97695c660bc38a9f55d9843dc2a847b
georgeankumensah <georgeankumensah@gmail.com>
2023-10-04 03:13:27 -0700

    updated server

--- index.js
+++ index.js
@@ -5,35 +5,29 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
 const flash = require("connect-flash");
-const dotenv = require("dotenv");
 const cors = require("cors")
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 dotenv.config();
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 mongoose.connection.on('error', (error) => console.error('MongoDB connection error:', error));
 mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
 
 // Middleware
 app.use(bodyParser.json());
-app.use(
-  session({
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: false,
-    
-  })
-);
-app.use(cors())
+app.use(cors());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false,
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
@@ -51,7 +45,6 @@ const orderRoutes = require('./routes/orderRoutes');
 const adminRoutes = require('./routes/adminRoutes'); // Include admin routes
 app.use('/orders', orderRoutes);
 app.use('/admin', adminRoutes); // Use /admin prefix for admin routes
-
 
 // Add admin route handlers here (e.g., for user registration, login, and admin actions)
 
